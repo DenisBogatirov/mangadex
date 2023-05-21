415ed8a4ba74ac9fda398edd3de63697c2f55f44
Denis Bogatirov <denis.bohatyrov@computools.com>
2023-05-22 01:06:06 +0300

    Implement Media Query Builder logic

--- lib/widgets/media_query_builder.dart
+++ lib/widgets/media_query_builder.dart
@@ -1,17 +1,41 @@
 // Flutter imports:
 import 'package:flutter/material.dart';
 
+// Package imports:
+import 'package:collection/collection.dart';
+
 class MediaQueryBuilder extends StatelessWidget {
   final WidgetBuilder fallback;
+  final Map<int, WidgetBuilder>? stops;
 
   const MediaQueryBuilder({
     super.key,
     required this.fallback,
+    this.stops,
   });
 
-  // TODO: Implement media query points
   @override
   Widget build(BuildContext context) {
+    final stops = this.stops;
+
+    if (stops != null && stops.isNotEmpty) {
+      final width = MediaQuery.of(context).size.width;
+      final sorted = stops.keys.sorted((a, b) => a - b);
+
+
+      if (width < sorted.first) {
+        return fallback(context);
+      }
+
+      for (final stop in sorted) {
+        if (width < stop) {
+          return stops[stop]!(context);
+        }
+      }
+
+      return stops[sorted.last]!(context);
+    }
+
     return fallback(context);
   }
 }
